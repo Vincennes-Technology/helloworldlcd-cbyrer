ba58f2896aa38c69c74c0872d1dcb52ab2ea5330
cbyrer <claytonbyrer@gmail.com>
2018-03-19 14:12:37 -0400

    55555

--- helloLCD.py
+++ helloLCD.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python
 #show "Hello World on the LCD screen""
 # CByrer
-
+import subprocess
 import Adafruit_CharLCD as LCD
+import time
 
 lcd = LCD.Adafruit_CharLCDPlate()
 
-displayText = "Hello World"
+Name = subprocess.check_output(['hostname']).strip()
+displayText = Name
 
-lcd.clear()
-lcd.message(displayText)
+IP = subprocess.check_output(["hostname", "-I"])
+refresh = True
+
+
+while (True):
+    if lcd.is_pressed(LCD.SELECT):
+        lcd.clear()
+        lcd.message(displayText + "\n")
+        lcd.set_backlight(1)
+        lcd.message("Hello World\n")
+        refresh = True
+        time.sleep(1)
+    else:
+        if refresh:
+            lcd.clear()
+            lcd.set_backlight(1)
+            lcd.message(displayText + "\n")
+            lcd.message(IP)
+            time.sleep(1)
+            refresh = False
+
 
